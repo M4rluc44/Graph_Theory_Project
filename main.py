2ad1bdbd2d904a469ead33707fb92c88bc85cf49
M4rluc44 <marinpierrebabin@gmail.com>
2021-12-02 12:31:18 +0100

    looping weeeeezzzz

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ exit = True
 while exit:
     print("Which graph do you wan to test ? Choose a number between 1 and 13")
     num = int(input())
+    result = functions.floyd_warshall(test_graph.graph_test[num - 1])
 
-    result = functions.floyd_warshall(test_graph.graph_test[num-1])
+    print("\nInitial graph :")
+    for i in range(len(test_graph.graph_test[num - 1])):
+        print("")
+        for j in range(len(test_graph.graph_test[num - 1][i])):
+            print(test_graph.graph_test[num - 1][i][j], "|", end=' ')
 
+    print("\nFloyd Warshall graph")
     for i in range(len(result)):
         print("")
         for j in range(len(result[i])):
