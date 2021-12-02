12138e95ae8dd899f68d7d6cc3d489c3852814c4
M4rluc44 <marinpierrebabin@gmail.com>
2021-12-02 12:16:14 +0100

    update ligne inutiles

--- functions.py
+++ functions.py
@@ -2,11 +2,7 @@ def floyd_warshall(graph):
     for i in range(len(graph)):
         for j in range(len(graph[i])):
             for k in range(len(graph[i])):
-                if graph[i][j] == 'Inf' and graph[i][k] == 'Inf' and graph[k][j] != 'Inf':
-                    graph[i][j] = graph[k][j]
-                elif graph[i][j] == 'Inf' and graph[k][j] == 'Inf' and graph[i][k] != 'Inf':
-                    graph[i][j] = graph[i][k]
-                elif graph[i][j] > graph[i][k] + graph[k][j]:
+                if graph[i][j] > graph[i][k] + graph[k][j]:
                     graph[i][j] = graph[i][k] + graph[k][j]
 
     return graph
