76ebf66b2e55667655b0c54e6d40268950d0cf26
M4rluc44 <marinpierrebabin@gmail.com>
2022-01-02 10:36:30 +0100

    explication comment rentrer test graph

--- test_graph.py
+++ test_graph.py
@@ -1,10 +1,16 @@
 import math
 Inf = math.inf
 
-graph_test = [  [[0, 1, 5, Inf],
-                [Inf, 0, -3, 5],
-                [Inf, Inf, 0, 2],
-                [Inf, Inf, Inf, 0]]
-        ]
+# Enter the relation table of the graph,
+# For example, set a 0 when it's a relation with the same relation of 1 with 1 is none, so 0 here
+# And put Inf when the relation doesn't exits between twoo (no relation between 1 and 2 ? so put Inf)
+
+graph_test = [   [[0, 1, 5, Inf], # test graph 1
+                 [Inf, 0, -3, 5],
+                 [Inf, Inf, 0, 2],
+                 [Inf, Inf, Inf, 0]]
+
+
+            ]
 
 
